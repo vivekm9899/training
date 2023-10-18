53904adc36dfbe2f08ba3ca234dd358f18d2fa0f
vivekm9899 <148049707+vivekm9899@users.noreply.github.com>
2023-10-18 07:32:58 +0530

    my first main

---
+++ NameAge.java
@@ -0,0 +1,20 @@
+class NameAge{ 
+
+public void printNameAge(String name,int age){
+System.out.println("Name"+name);
+System.out.println("Age" +age);
+
+}
+public static void main(String args[]){
+NameAge object=new NameAge();
+object.printNameAge("vivek", 26);
+
+}
+
+
+}
+
+
+
+
+
