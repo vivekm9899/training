e8dc021fc39aa37f9d0f54f596878a770e42879f
USER <USER@192.168.1.40>
2023-10-20 07:19:15 +0530

    first commit

---
+++ src/com/obsqura/programms/Sum.java
@@ -0,0 +1,14 @@
+package com.obsqura.programms;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=5;
+int sum=a+b;
+System.out.println("sum of a and b is"+sum+"");
+
+	}
+
+}
