713a24472ea8be14976a696c75082cf08b007888
USER <USER@192.168.1.40>
2023-10-25 06:53:34 +0530

    second commit

---
+++ src/com/obsqura/programms/Addition.java
@@ -0,0 +1,13 @@
+package com.obsqura.programms;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=15;
+float b=10.2f;
+System.out.println(a+b);
+
+	}
+
+}
---
+++ src/com/obsqura/programms/Method.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.obsqura.programms;
+
+/**
+ * 
+ */
+class Method {
+public void display(){
+	System.out.println("Method");	
+}{
+	
+}	
+/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
